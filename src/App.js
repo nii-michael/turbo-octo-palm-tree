ba904be7cc36eb7cb44d905563966f63c42fea71
tylerasa <sarpongasaresylvester@gmail.com>
2022-04-27 00:17:54 +0000

    +entries

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
 import Dashboard from "./pages/Dashboard/Dashboard";
+import Entries from "./pages/Entries/Entries";
 import Login from "./pages/Login/Login";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route exact path="/"  element={<Login/>} />
-        <Route exact path="/dashboard" element={<Dashboard/>} />
+        <Route exact path="/" element={<Login />} />
+        <Route exact path="/dashboard" element={<Dashboard />} />
+        <Route exact path="/entries" element={<Entries />} />
       </Routes>
     </BrowserRouter>
   );
---
+++ src/pages/Entries/Entries.js
@@ -0,0 +1,17 @@
+import React from "react";
+import DashboardMainWrapper from "../../components/DashboardMainWrapper";
+import DashboardWrapper from "../../components/DashboardWrapper";
+import './styles.css'
+const Entries = () => {
+  return (
+    <DashboardWrapper>
+      <DashboardMainWrapper name="Entries">
+        <div className="accounts-wrapper">
+        
+        </div>
+      </DashboardMainWrapper>
+    </DashboardWrapper>
+  );
+};
+
+export default Entries;
