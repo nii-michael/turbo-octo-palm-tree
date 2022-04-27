0377a627f5ace583f5c78d66b1c91a0c72f255da
tylerasa <sarpongasaresylvester@gmail.com>
2022-04-27 08:58:17 +0000

    +userSlice, store

---
+++ src/redux/store.js
@@ -0,0 +1,30 @@
+import { configureStore } from "@reduxjs/toolkit";
+import storage from "redux-persist/lib/storage";
+import { combineReducers } from "redux";
+import {
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER
+} from "redux-persist";
+import getTimerReducer from "./getTimeSlice";
+
+const persistConfig = {
+  key: "user",
+  storage
+};
+const reducers = combineReducers({ user: getTimerReducer });
+const persistedReducer = persistReducer(persistConfig, reducers);
+
+export const store = configureStore({
+  reducer: persistedReducer,
+  middleware: getDefaultMiddleware =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER]
+      }
+    })
+})
