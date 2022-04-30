b3c23fb95eb31c972691bc56c91e015519dd1d48
tylerasa <sarpongasaresylvester@gmail.com>
2022-04-30 12:31:54 +0000

    +purge to null

--- src/pages/Dashboard/Dashboard.js
+++ src/pages/Dashboard/Dashboard.js
@@ -2,8 +2,15 @@ import React from "react";
 import DashboardMainContent from "../../components/DashboardMainContent";
 import DashboardMainWrapper from "../../components/DashboardMainWrapper";
 import DashboardWrapper from "../../components/DashboardWrapper";
+import { useSelector } from "react-redux";
 
 const Dashboard = () => {
+  const token = useSelector(state => state.user.access_token);
+
+
+  if(!token){
+    
+  }
   return (
     <DashboardWrapper>
       <DashboardMainWrapper name="Home">
