a6ad8044346a1ce84583fb5ef492ebd49cd9e6b2
tylerasa <sarpongasaresylvester@gmail.com>
2022-04-30 11:06:37 +0000

    +reports route

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import "./App.css";
 import Dashboard from "./pages/Dashboard/Dashboard";
 import Entries from "./pages/Entries/Entries";
 import Login from "./pages/Login/Login";
+import Reports from "./pages/Reports/Reports";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <Route exact path="/" element={<Login />} />
         <Route exact path="/dashboard" element={<Dashboard />} />
         <Route exact path="/dashboard/entries" element={<Entries />} />
+        <Route exact path="/dashboard/reports" element={<Reports />} />
       </Routes>
     </BrowserRouter>
   );
